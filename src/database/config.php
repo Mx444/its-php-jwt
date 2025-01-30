879414677c3e8a273e96dffdcbda9dfb390214e9
Mx444 <mybbtester@gmail.com>
2025-01-30 16:06:17 +0100

    config: database config

--- src/database/config.php
+++ src/database/config.php
@@ -3,16 +3,19 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../../');
 $dotenv->load();
 
-$servername = $_ENV['DB_HOST'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASSWORD'];
-$dbname = $_ENV['DB_NAME'];
+define('DB_HOST', $_ENV['DB_HOST']);
+define('DB_USER', $_ENV['DB_USER']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_NAME', $_ENV['DB_NAME']);
 
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connessione fallita: " . $conn->connect_error);
+function getDbConnection(){
+    try{
+        return new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    }catch(Exception $error){
+        echo "Error: " . $error->getMessage();
+    }
 }
-echo "Connessione riuscita : $conn->host_info" ;
-?>
 
+
+getDbConnection();
