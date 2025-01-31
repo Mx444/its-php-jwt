370b39d5fa56e85ef4a518ccd0e11a093d8c1300
Mx444 <mybbtester@gmail.com>
2025-01-31 10:08:18 +0100

    feat: added update and delete method

--- src/auth/repositories/auth.repository.php
+++ src/auth/repositories/auth.repository.php
@@ -11,7 +11,7 @@ class AuthRepository
 
     public function findByEmail($email)
     {
-        $query = "SELECT * FROM auth WHERE email = ?";
+        $query = "SELECT * FROM auth WHERE email = ? AND deleted_at IS NULL";
         $stmt = $this->db->prepare($query);
         $stmt->bind_param("s", $email);
         $stmt->execute();
@@ -38,5 +38,38 @@ class AuthRepository
         $insertedId = $stmt->insert_id;
         $stmt->close();
         return $insertedId;
+    }
+
+    public function update($id, $col, $hashedPassword)
+    {
+        $col = $col == 'password' ? 'password' : 'email';
+        $query = "UPDATE auth SET $col = ?, updated_At = NOW() WHERE id = ? AND password = ?";
+        $stmt = $this->db->prepare($query);
+        if ($stmt === false) {
+            throw new Exception("Failed to prepare the query: " . $this->db->error);
+        }
+        $stmt->bind_param("is", $id, $hashedPassword);
+        if (!$stmt->execute()) {
+            throw new Exception("Execution failed: " . $stmt->error);
+        }
+        $updatedRows = $stmt->affected_rows;
+        $stmt->close();
+        return $updatedRows;
+    }
+
+    public function delete($id, $email, $hashedPassword)
+    {
+        $query = "UPDATE auth SET deleted_at = NOW() WHERE id = ? AND email = ? AND password = ?";
+        $stmt = $this->db->prepare($query);
+        if ($stmt === false) {
+            throw new Exception("Failed to prepare the query: " . $this->db->error);
+        }
+        $stmt->bind_param("iss", $id, $email, $hashedPassword);
+        if (!$stmt->execute()) {
+            throw new Exception("Execution failed: " . $stmt->error);
+        }
+        $deletedRows = $stmt->affected_rows;
+        $stmt->close();
+        return $deletedRows;
     }
 }
